d6ab7d8fb6e1dd2b3f6bb54d0f3bcad27d402a18
Gustavo <drgustav1997@gmail.com>
2020-12-12 15:30:20 -0300

    _futbr

---
+++ buton.js
@@ -0,0 +1,13 @@
+function expandeBusca(elemento){
+    elemento.classList.add('aberto');
+    elemento.classList.remove('fechado');
+    document.getElementById('titulo-site').classList.remove('visivel');
+    document.getElementById('titulo-site').classList.add('invisivel');
+}
+
+function fechaBusca(elemento){
+    elemento.classList.add('fechado');
+    elemento.classList.remove('aberto');
+    document.getElementById('titulo-site').classList.remove('invisivel')
+    document.getElementById('titulo-site').classList.add('visivel')
+}
